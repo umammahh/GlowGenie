5e90bba5cd39de4ef71ef3f545f50ef49df9ab18
Umama Qasim <90606124+umammahh@users.noreply.github.com>
2025-04-25 11:14:56 +0400

    GitHub workflow (#2)
    * home page and navbar basics done
    
    * running workflows
    
    ---------
    
    Co-authored-by: umama.qasim <umama.qasim@stech.ai>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,8 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-
+import Home from "./pages/Home";
 function App() {
-  const [msg, setMsg] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:8000/api/test/")
-      .then((res) => setMsg(res.data.message))
-      .catch((err) => console.error(err));
-  }, []);
-
   return (
     <div>
-      <h1>Glow Genie ✨</h1>
-      <p>{msg}</p>
+      <Home />
     </div>
   );
 }
